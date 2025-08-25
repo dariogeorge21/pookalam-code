038602ba6dab9b4a031741e1eb027fa66da76631
Dario George <edu.dariogeorge21@gmail.com>
2025-08-25 12:16:09 +0530

    cleanup

--- pookalam.py
+++ pookalam.py
@@ -1,8 +1,5 @@
-# Open the file in read mode
 with open("pookalam.txt", "r") as file:
-    # Iterate over each line in the file
     for line in file:
-        line = line.strip("\n")   # remove only the newline at end
-        # join characters with space
+        line = line.strip("\n")  
         spaced_line = " ".join(line)
         print(spaced_line)
