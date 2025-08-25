ff603a2ef04e601ff8dba15662a324f193a64c2b
Dario George <edu.dariogeorge21@gmail.com>
2025-08-25 12:15:19 +0530

    update href

--- pookalam.py
+++ pookalam.py
@@ -1,5 +1,5 @@
 # Open the file in read mode
-with open("plan3 without.txt", "r") as file:
+with open("pookalam.txt", "r") as file:
     # Iterate over each line in the file
     for line in file:
         line = line.strip("\n")   # remove only the newline at end
